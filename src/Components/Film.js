514dc5d971b912ee597076e686ef147fe02af509
jordanperrin <perrinjordan@gmail.com>
2024-03-20 07:42:07 -0400

    final animation

--- src/Components/Film.js
+++ src/Components/Film.js
@@ -16,31 +16,51 @@ export function Film(props) {
   const { nodes, materials } = useGLTF('/film.glb');
   const film = useRef();
   const scroll = useScroll();
-  const tl = useRef();  
+  const tl = useRef();
 
 
-  useFrame((state, delta) =>{
+  useFrame((state, delta) => {
     tl.current.seek(scroll.offset * tl.current.duration())
   })
-  
-  useLayoutEffect(() =>{
-    tl.current = gsap.timeline({defaults: {duration: 1}})
-  
+
+  useLayoutEffect(() => {
+    tl.current = gsap.timeline({ defaults: { duration: 1 } })
+
     tl.current
-      .from(film.current.position, { x:-1.5}, 0 )
-      // .from(film.current.scale, { x:-.3, y:-.3, z:-.3}, 0 )
-      // .from(film.current.rotation, { x:0.8, y:0, z:0},0)
+      .from(film.current.position,
+         { 
+          x: -1
+         }
+         , 0)
+      .from(film.current.scale,
+         {
+          duration:1,
+          x: -.3,
+          y: -.3, 
+          z: -.3 
+        },
+         0)
+      .from(film.current.rotation,
+        {
+          duration: 1,
+          x: Math.PI / 1,
+          y: -Math.PI / 0.8,
+          z: Math.PI/0.8
+        },
+        0
+      );
 
-      
 
 
-  
-  },[])
+
+
+
+  }, [])
 
   return (
-    <group {...props} dispose={null} ref = {film}>
+    <group {...props} dispose={null} ref={film}>
       <group scale={0.023}>
-        <mesh geometry={nodes['LP_roll_film_Material_#27_0'].geometry} material={materials.Material_27} rotation={[-Math.PI / 1.9, 0, -.3]} position={[23, -12, 0]}/>
+        <mesh geometry={nodes['LP_roll_film_Material_#27_0'].geometry} material={materials.Material_27} rotation={[-Math.PI / 1.9, 0, -.3]} position={[23, -12, 0]} />
       </group>
     </group>
   )
